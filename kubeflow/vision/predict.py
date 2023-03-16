e99a29dcd434014ab41b536c29df3003564f3bf6
Pravin Reddy <pravin@duplocloud.net>
2023-03-16 16:09:18 -0700

    kubeflow install destroy scripts

--- kubeflow/vision/predict.py
+++ kubeflow/vision/predict.py
@@ -7,6 +7,21 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import imagenet_utils
+from sklearn.metrics import confusion_matrix
+import itertools
+import os
+import shutil
+import random
+import matplotlib.pyplot as plt
+
 
 
 # map to external drive
@@ -23,19 +38,33 @@ test_dir = f"{data_root_path}/test-images"
 model_name="MobileNetV3(small)"
 model_path = f"{models_dir}/{model_name}.h5"
 
+def prepare_image(file):
+    img = image.load_img(test_dir + file, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+def get_test_batches():
+    return ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+        directory=test_dir,
+        target_size=(224, 224),
+        batch_size=10,
+        classes=['cans', 'colorless_pet', 'glass', 'nothing', 'paper', 'plastic']
+    )
+
+
 #Read model
 model = load_model(model_path)
 
-test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
-    directory=test_dir,
-    target_size=(224, 224),
-    batch_size=10,
-    classes=['cans', 'colorless_pet', 'glass', 'nothing', 'paper', 'plastic']
-)
 
-#Prediction
-predictions=model.predict(x=test_batches, verbose=0)
+# Prediction test_batches
+test_batches = get_test_batches()
+predictions = model.predict(x=test_batches, verbose=0)
 np.round(predictions) # to 1 or 0
 cm = confusion_matrix(y_true=test_batches.classes,y_pred=np.argmax(predictions, axis=-1))
-
 print(cm)
+
+
+preprocessed_image = prepare_image('1.jpg')
+predictions_single = model.predict(preprocessed_image)
+print(predictions_single)
