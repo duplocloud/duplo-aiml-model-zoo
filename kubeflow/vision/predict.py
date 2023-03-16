ebfa454ca7078fbf390fc1c2fb91249c034e5523
Pravin Reddy <pravin@duplocloud.net>
2023-03-16 16:56:20 -0700

    kubeflow install destroy scripts

--- kubeflow/vision/predict.py
+++ kubeflow/vision/predict.py
@@ -1,28 +1,15 @@
 import pathlib
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image_dataset_from_directory
 import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
-from sklearn.metrics import confusion_matrix
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
 from tensorflow.keras.applications import imagenet_utils
-from sklearn.metrics import confusion_matrix
-import itertools
 import os
-import shutil
-import random
-import matplotlib.pyplot as plt
-
-
 
 # map to external drive
 MAPPED_PATH = os.getenv("MAPPED_PATH", default = "./imagedata")
@@ -38,20 +25,30 @@ test_dir = f"{data_root_path}/test-images"
 model_name="MobileNetV3(small)"
 model_path = f"{models_dir}/{model_name}.h5"
 
+classes=['cans', 'colorless_pet', 'glass', 'nothing', 'paper', 'plastic']
+
 def prepare_image(file):
-    img = image.load_img(test_dir + file, target_size=(224, 224))
+    img = image.load_img( f"{data_root_path}/test-images/{file}", target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
 
 def get_test_batches():
     return ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
-        directory=test_dir,
+        directory= test_dir,
         target_size=(224, 224),
         batch_size=10,
-        classes=['cans', 'colorless_pet', 'glass', 'nothing', 'paper', 'plastic']
+        classes=classes
     )
 
+
+def get_all_image_batches():
+    return ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+        directory=data_dir, #test_dir,
+        target_size=(224, 224),
+        batch_size=10,
+        classes=classes
+    )
 
 #Read model
 model = load_model(model_path)
@@ -60,11 +57,22 @@ model = load_model(model_path)
 # Prediction test_batches
 test_batches = get_test_batches()
 predictions = model.predict(x=test_batches, verbose=0)
-np.round(predictions) # to 1 or 0
-cm = confusion_matrix(y_true=test_batches.classes,y_pred=np.argmax(predictions, axis=-1))
-print(cm)
+np.round(predictions)
+print(classes,"\n", predictions)
 
+
+#
+# all_image_batches = get_all_image_batches()
+# predictions = model.predict(x=all_image_batches, verbose=0)
+# np.round(predictions) # to 1 or 0
+# results = tf.keras.applications.mobilenet.decode_predictions(
+#     predictions, top=5
+# )
+# # results = imagenet_utils.decode_predictions(predictions) # needs 20 bqtch
+# print(results)
+# print(classes,"\n", predictions)
 
 preprocessed_image = prepare_image('1.jpg')
 predictions_single = model.predict(preprocessed_image)
-print(predictions_single)
+#Let's predict top 5 results
+print(classes,"\n", predictions_single)
