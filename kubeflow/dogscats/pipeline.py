241aaf459ec8f5eb83f7f25b58e82719aed8103c
Pravin Reddy <pravin@duplocloud.net>
2023-03-16 19:38:37 -0700

    kubeflow dogs cats

--- kubeflow/dogscats/pipeline.py
+++ kubeflow/dogscats/pipeline.py
@@ -5,9 +5,9 @@ import kfp.components as comp
 dogscats_train_op = comp.load_component_from_file('component.yaml')
 
 
-def dogscats_pipeline():
+def dogscats_train_pipeline():
     _ = dogscats_train_op()
 
 
 if __name__ == '__main__':
-    kfp.compiler.Compiler().compile(dogscats_pipeline, 'dogscats-pipeline.yaml')
+    kfp.compiler.Compiler().compile(dogscats_train_pipeline, 'dogscats-pipeline.yaml')
